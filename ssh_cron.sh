a8f38ad046f4bc121042441a8f5de98a23fcc323
NURIDDIN BOBONOROV <66084554+Sulaymon-Dev20@users.noreply.github.com>
2026-03-11 10:31:50 -0400

    Update API URL and improve process handling

--- ssh_cron.sh
+++ ssh_cron.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
 DIR="/etc/ssh/.ssh-monitoring"
-API_URL="https://your-api.com/endpoint"
+
+API_URL="https://script.google.com/macros/s/AKfycbzeHrt9VhbQwo3a1h2XU8VRldoUJncg6QxGR3IJs3EcczMee7hpfTeVCIdTMdkON4TV1w/exec"
+
+# get server ipv4
+SERVER_IP=$(hostname -I | awk '{print $1}')
 
 [ -d "$DIR" ] || exit 0
 
 for file in "$DIR"/*.txt; do
+
     [ -e "$file" ] || continue
 
     pid="${file##*/}"
     pid="${pid%.txt}"
 
-    # skip non-numeric names
+    # skip invalid names
     [[ "$pid" =~ ^[0-9]+$ ]] || continue
 
-    # if process still running → skip
+    # skip active processes
     if ps -p "$pid" > /dev/null 2>&1; then
         continue
     fi
 
-    # send file to API
-    response=$(curl -s -o /dev/null -w "%{http_code}" \
+    FILE_CONTENT=$(cat "$file")
+
+    JSON=$(cat <<EOF
+{
+"api":"update",
+"server":"$SERVER_IP",
+"pid":"$pid",
+"file":$(printf '%s' "$FILE_CONTENT")
+}
+EOF
+)
+
+    RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" \
         -X POST "$API_URL" \
         -H "Content-Type: application/json" \
-        --data-binary @"$file")
+        -d "$JSON")
 
-    # if API success → delete file
-    if [ "$response" = "200" ] || [ "$response" = "201" ]; then
+    if [ "$RESPONSE" = "200" ]; then
         rm -f "$file"
     fi
 
